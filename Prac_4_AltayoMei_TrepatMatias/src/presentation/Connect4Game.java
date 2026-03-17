9c3d0f2fb49752c185415e1afa63e914cfa53030
Mati275 <matitrepat@gmail.com>
2026-03-17 15:09:32 +0100

    Acabada la segunda parte de la practica 4

--- Prac_4_AltayoMei_TrepatMatias/src/presentation/Connect4Game.java
+++ Prac_4_AltayoMei_TrepatMatias/src/presentation/Connect4Game.java
@@ -1,18 +1,22 @@
 package presentation;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -20,14 +24,19 @@ import domain.Game;
 
 public class Connect4Game extends JFrame implements ActionListener{
 	
-	//Declarar paneles
 	
+	// ATTRIBUTES
 	private JButton [][] gridButton;
 	private Game game;
 	private JButton newGameButton, closeButton;
 	private ImageIcon playerX, playerO;	
 	
+	private String playerXName;
+	private String playerOName;
+	private int moveCounter;
+	private JLabel moveCounterNumberLabel;
 	
+	// CONSTRUCTOR
 	public Connect4Game (String message) {
 		super(message);
 		
@@ -36,26 +45,40 @@ public class Connect4Game extends JFrame implements ActionListener{
 	
 	
 	
+	/**
+	 * Positioning the containers and creating all the components inside them
+	 * This method goes inside the constructor, allowing the creation of all the components only creating this object
+	 */
 	private void initComponents() { 	
-		JPanel gridContainer, btnContainer;
-		Dimension windowDimension, dimBtnContainer;
+		
+		// Local variables 
+		JPanel gridContainer, btnContainer, playerInfoContainer;
+		Dimension windowDimension, dimBtnContainer, dimPlayerInfoContainer;
+		JLabel labelP1, labelP1Name, labelP2, labelP2Name, labelMove;
+		Font font, boldFont;
 
 		// Set close operation --> EXIT_ON_CLOSE
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		
+		playerInfoContainer = new JPanel();
 		gridContainer = new JPanel();
 		btnContainer = new JPanel();
 		game = new Game();
 		
 		// POSITIONING THE CONTAINERS
 		
-		// Main JFrame
+		// Container of main JFrame
 		this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS) );
 		
 		windowDimension = new Dimension (800,600); // Value of pixels of the JFrame
 		setSize(windowDimension);			
 		setMinimumSize(windowDimension);	
+		
+		// playerInfoContainer
+		dimPlayerInfoContainer = new Dimension (Integer.MAX_VALUE, 70);
+		playerInfoContainer.setMaximumSize(dimPlayerInfoContainer);
+		this.getContentPane().add(playerInfoContainer);		
+
 		
 		// gridContainer
 		gridContainer.setLayout(new GridLayout(game.getBoardRow(), game.getBoardCol()));
@@ -73,6 +96,51 @@ public class Connect4Game extends JFrame implements ActionListener{
 		
 		this.getContentPane().add(btnContainer);
 		
+		// ASK NAME + ADDING THE LABELS
+		
+		// Ask name
+		playerXName = JOptionPane.showInputDialog("Nombre jugador 1: ");
+		
+		playerOName = JOptionPane.showInputDialog("Nombre jugador 2: ");
+		
+		// Create and add the labels & fonts
+		
+		// Creating the fonts
+		font = new Font (Font.SANS_SERIF, Font.PLAIN, 20); 		// Normal font
+		boldFont = new Font (Font.SANS_SERIF, Font.BOLD, 20);	// Bold font
+		
+		// Creating the labels
+		labelP1 = new JLabel("Player 1: ");
+		labelP1Name = new JLabel(playerXName);
+		labelP2 = new JLabel("Player 2");
+		labelP2Name = new JLabel(playerOName);
+		labelMove = new JLabel("Move counter");
+		moveCounterNumberLabel = new JLabel(Integer.toString(moveCounter));
+		   
+		// Setting the font to the labels
+		labelP1.setFont(boldFont);
+		labelP1Name.setFont(font);
+		
+		labelP2.setFont(boldFont);
+		labelP2Name.setFont(font);
+
+		labelMove.setFont(boldFont);
+		moveCounterNumberLabel.setFont(font);
+		moveCounterNumberLabel.setForeground(Color.RED);
+		
+		// Adding the labels
+		playerInfoContainer.add(labelP1);
+		playerInfoContainer.add(labelP1Name);
+		playerInfoContainer.add(Box.createHorizontalStrut(20));
+
+		
+		playerInfoContainer.add(labelP2);
+		playerInfoContainer.add(labelP2Name);
+		playerInfoContainer.add(Box.createHorizontalStrut(30));
+
+		playerInfoContainer.add(labelMove);
+		playerInfoContainer.add(moveCounterNumberLabel);
+		
 		
 		// CREATING THE BUTTONS, CONFIGURE THEM AND ADDING THEM TO THE GUI
 		newGameButton = new JButton ("NEW GAME");
@@ -85,15 +153,9 @@ public class Connect4Game extends JFrame implements ActionListener{
 				gridButton[i][j] = new JButton(); 
 				
 				gridButton[i][j].setContentAreaFilled(false); // Flat the button (remove the background)
-				
+				gridButton[i][j].setOpaque(true);
+
 				gridButton[i][j].setName(i + "," + j); // Setting a name (the same as "ID") to identify the row and column of each button
-				
-				// TODO: ASK
-				// Set the last button row enabled, otherwise, disable them
-				
-//				if( i != game.getBoardRow() - 1){
-//					gridButton[i][j].setEnabled(false);
-//				}
 				
 				gridButton[i][j].addActionListener(this); // Adding a listener to the event of clicking this button
 				
@@ -106,26 +168,24 @@ public class Connect4Game extends JFrame implements ActionListener{
 		// Creating the button and adding a listener for the ActionEvent
 		newGameButton = new JButton("New game");
 		newGameButton.addActionListener(this);
-
-		// TODO: ASK (IF IT'S BETTER TO PUT THE NAME OR NOT)
-		//newGameButton.setName("newGameButton");
 		
 		// Creating the button and adding a listener for the ActionEvent
 		closeButton = new JButton("Close");
 		closeButton.addActionListener(this);
-		//closeButton.setName("closeButton");
 		
 		// Adding the buttons to their container
 		btnContainer.add(newGameButton);
 		btnContainer.add(closeButton);
 
 		
-		
-		setVisible(true); // Set visible the JFrame 
-		
-		// Create the imageIcon for each player
-		playerX = new ImageIcon( new ImageIcon("img/PlayerX.jpg").getImage().getScaledInstance(gridButton[0][0].getBounds().width, gridButton[0][0].getBounds().height, Image.SCALE_SMOOTH)    );
-		playerO = new ImageIcon( new ImageIcon("img/PlayerO.jpg").getImage().getScaledInstance(gridButton[0][0].getBounds().width, gridButton[0][0].getBounds().height, Image.SCALE_SMOOTH)    );
+        this.setVisible(true);
+
+		// Create the imageIcon for each player and scale the images for having the same size as the buttons
+        
+        // As the parameter of the first constructor it's has the scaled image, it's obtained 
+        // by creating another imageIcon and getting it's image scaled (with the same size as the button at the start)
+		playerX = new ImageIcon( new ImageIcon("img/PlayerX.png").getImage().getScaledInstance(gridButton[0][0].getBounds().width, gridButton[0][0].getBounds().height, Image.SCALE_SMOOTH)    );
+		playerO = new ImageIcon( new ImageIcon("img/PlayerO.png").getImage().getScaledInstance(gridButton[0][0].getBounds().width, gridButton[0][0].getBounds().height, Image.SCALE_SMOOTH)    );
 		
 		
 		
@@ -140,7 +200,7 @@ public class Connect4Game extends JFrame implements ActionListener{
 		
 		JButton but;
 		
-		// Cheking if the element which is clicked is a JButton
+		// Checking if the element which is clicked is a JButton
 		if( (e.getSource() instanceof JButton) ) {
 			
 			but = (JButton) e.getSource();
@@ -188,7 +248,11 @@ public class Connect4Game extends JFrame implements ActionListener{
 				butRow = Integer.parseInt(strCoordinates[0]);
 				butCol = Integer.parseInt(strCoordinates[1]);
 				
-				game.move(butCol); // 2. Make the move (in the logic of the game)
+				// 2. Make the move (in the logic of the game)
+				
+				game.move(butCol); 
+				moveCounter ++;
+				moveCounterNumberLabel.setText(Integer.toString(moveCounter));
 				
 				// 3. Make the move (in the GUI) --> Set an image to represent that the player made a move
 				if(game.whoMoved(butRow, butCol) == 'X' ) {
@@ -205,13 +269,14 @@ public class Connect4Game extends JFrame implements ActionListener{
 
 				//setValidButton(butCol); // Call the function that enables/disables the buttons following the logic of the game
 				
-				// 4. Disable and enable the correct buttons
-				// TODO: ASK
-				
+				// 4. Disable and enable the correct buttons + changing their color
 				gridButton[butRow][butCol].setEnabled(false);
+				gridButton[butRow][butCol].setBackground(new Color(238, 238, 238));
+				
 				// If the button which is clicked isn't on the first row --> Enable the button that is over the button which is currenly clicked
 				if( butRow > 0 ) {
 					gridButton[butRow - 1][butCol].setEnabled(true);
+					gridButton[butRow -1][butCol].setBackground(Color.WHITE);
 				}
 				
 				
@@ -239,40 +304,7 @@ public class Connect4Game extends JFrame implements ActionListener{
 	// ************
 	// EXTRA METHODS (private methods)
 	// *************
-	
-	/**
-	 * Called just after making a move, in order to enable and disable teh 
-	 * @param row
-	 * @param col
-	 */
 
-//	private void setValidButton(int row, int col) {
-//		
-//		gridButton[row][col].setEnabled(true);
-//		gridButton[row + 1][col].setEnabled(false);
-		
-//		boolean found = false; // Indicates if there's found a null value or not
-//		
-//		for(int i = game.getBoardRow() - 2; i >= 0 && !found; i-- ) {
-//			
-//			// If there's some button which isn't filled
-//			if( game.whoMoved(i, col) == ' ' ) {
-//				
-//				gridButton[i][col].setEnabled(true);
-//				gridButton[i + 1][col].setEnabled(false);
-//				found = true;
-//				
-//				//return;
-//			}
-//			
-//		} 
-//		
-//		// All the buttons are filled
-//		if( !found ) {
-//			gridButton[0][col].setEnabled(false);
-//		}
-		
-		
 		
 	/**
 	 * Called when the user wants to reset the game, reseting all the values of the logic of the game and the GUI 
@@ -280,64 +312,50 @@ public class Connect4Game extends JFrame implements ActionListener{
 	private void resetGame() {
 		
 		game = new Game(); // Make another game
+		moveCounter = 0;
+		moveCounterNumberLabel.setText(Integer.toString(moveCounter));
 		
-		
-		for (int i = 0; i < gridButton.length; i++) {		//Crear las celdas
+		// Setting all the buttons the default icon (without image)
+		for (int i = 0; i < gridButton.length; i++) {		
 			for (int j = 0; j < gridButton[i].length; j++) {
 				gridButton[i][j].setIcon(null); 
 				gridButton[i][j].setDisabledIcon(null);
-			
-				//Activa los btn de la parte inferior
-//				if (i == game.getBoardRow() -1) {
-//					gridButton[i][j].setEnabled(true);
-//					
-//					
-//				} else {
-//					gridButton[i][j].setEnabled(false);
-//
-//				}
 			}
 		}
 		
-		restartBoard();
+		restartBoard(); // Set the correct enabled buttons
 	}
 	
 	
 	/**
 	 * Disables all the buttons of the array of gridButton except of the ones that are in the last row
 	 */	
+	
 	private void restartBoard() {
 		
 		for (int i = 0; i < gridButton.length - 1; i++) {		
 			for (int j = 0; j < gridButton[i].length; j++) {
 				gridButton[i][j].setEnabled(false); // Disable all the buttons except for the ones in the last row
+				gridButton[i][j].setBackground(new Color(238, 238, 238));
 			}
 		}
 			
 		for (int j = 0; j < gridButton[gridButton.length - 1].length; j++) {
 			gridButton[gridButton.length - 1][j].setEnabled(true); // Enable the buttons of the last row
+			gridButton[gridButton.length - 1][j].setBackground(Color.WHITE);
 		}
 		
 		
 	}
 		
-	
-	
-	
-	
-	
-	
 	public static void main (String args[]) {
 
         EventQueue.invokeLater(
                 new Runnable() {
 
-                	// TODO: ASK IF IT'S CORRECT CREATING THE JFRAME LIKE THIS
-                    @Override
+                	@Override
                     public void run() {
                         JFrame frame = new Connect4Game("Connecta 4");
-                        //frame.setSize(800,600);
-                        //frame.setVisible(true);
 
                     }
                 }
