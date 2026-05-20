be6fad678841c765694c7255940db578a1da72d8
Mati275 <matitrepat@gmail.com>
2026-05-20 20:51:57 +0200

    Practica 3 3r trimestre 1r año acabada
    Se ha acabado de comentar el codigo

--- 1r año - 3r Trim/EDA-P2-2026/src/tracker/AuthorshipTrackerMap.java
+++ 1r año - 3r Trim/EDA-P2-2026/src/tracker/AuthorshipTrackerMap.java
@@ -92,10 +92,13 @@ public class AuthorshipTrackerMap implements AuthorshipTracker{ //TODO: Complete
 	public SortedSet<Author> findAuthors(BookTag tag) {
 		SortedSet <Author> sortedAuthors = new TreeSet<>(new AuthorNameComparator());
 
-		
+		// Iterate each entry of the map
 		for (Map.Entry<Author,Set<Book>> entry : authorBookMap.entrySet()) {
+
+			// Obtain each book of each author
 			for (Book book : entry.getValue()) {
 
+				// If the bookTag is the same
 				if (book.getTag().equals(tag)) {
 					sortedAuthors.add(entry.getKey());
 
@@ -110,31 +113,35 @@ public class AuthorshipTrackerMap implements AuthorshipTracker{ //TODO: Complete
 	public SortedSet<Book> findBooks(String authorName) {
 		SortedSet<Book> sortedBook = new TreeSet<>( new BookTitleComparator() );
 
+		// Iterate the keySet of the map (a Set of all the authors )
 		for ( Author author : authorBookMap.keySet() ) {
 
+			// If the authorName (in parameters) is the same as one author on the map
 			if( author.getName().equalsIgnoreCase(authorName) ){
-				sortedBook.addAll(authorBookMap.get( author ));
+				sortedBook.addAll(authorBookMap.get( author )); // Add the books on "sortedBook" to sort the books of the author
 				return sortedBook;
 			}
 
 		}
 
-		return sortedBook;
+		return sortedBook; // Returns a void SortedSet
 	}
 
 	@Override
 	public SortedSet<Book> findBooksInCommon(String name1, String name2) {
 		SortedSet<Book> sortedBook = new TreeSet<>();
 
+		// Using the complimentary method to find the object of the author
 		Author author1 = findAuthorByName( name1 );
 		Author author2 = findAuthorByName( name2 );
+
 		Set<Book> author2Books = authorBookMap.get(author2);
 
-
-
+		// Iterate the books of the author1
 		for( Book book : authorBookMap.get(author1) ){
+			// If one of the books of the author2 is the same as one book of the author1
 			if( author2Books.contains(book) ){
-				sortedBook.add(book);
+				sortedBook.add(book); // Add the book to the sortedBook
 			}
 		}
 
